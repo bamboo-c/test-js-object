d32700cee905771caa08aa2831b0bfc0d057fd6b
Komei Otake <komei1220@gmail.com>
2014-08-08 00:51:59 +0900

    update development

---
+++ js/main.js
@@ -0,0 +1,15 @@
+// こうめオブジェクト
+
+var koume = {
+	name : "koume",
+	age : 23,
+	hobby : "football",
+	talk : function() {
+		console.log("ホモォ...");
+	}
+};
+
+// こうめオブジェクトへのアクセス
+console.log(koume.age);
+koume.talk();
+
